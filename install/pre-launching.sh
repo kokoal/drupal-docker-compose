bdbb1cd6a2c4ec38b179af95097e9f393b333a9b
Alan RIDARD <alan.ridard@gmail.com>
2019-09-23 23:14:17 +0000

    Correct pre-launching script

--- install/pre-launching.sh
+++ install/pre-launching.sh
@@ -20,8 +20,10 @@ if [ ${version_size} -lt ${drupal_max_version_number} ]
 then
     is_full_drupal_version=false;
     drupal_git_version="${drupal_git_version}${drupal_version_delimiter}x";
+    drupal_git_branch=${drupal_git_version};
 else
     is_full_drupal_version=true;
+    drupal_git_branch="${split_version[0]}${drupal_version_delimiter}${split_version[1]}${drupal_version_delimiter}x";
 fi
 
 # Set up Drupal github repository
@@ -29,26 +31,26 @@ drupal_github_repository_uri="https://github.com/drupal/drupal.git"
 # Set up tmp folder to use git clone
 drupal_tmp_folder_name="tmp_drupal";
 # Clone Drupal repository
-git clone --depth=1 ${drupal_github_repository_uri} ${drupal_tmp_folder_name};
-
+git clone -b ${drupal_git_branch} --single-branch  --depth=1 ${drupal_github_repository_uri} ${drupal_tmp_folder_name};
+# If the version is full checkout corresponding tag
 if [ ${is_full_drupal_version} = true ]
 then
-    cd ${drupal_tmp_folder_name};
+	cd ${drupal_tmp_folder_name};
     git checkout tags/${drupal_git_version};
-    cd ..;
+	cd ..;
 fi
 
-# Get composer file
+# Get composer files
 cat ${drupal_tmp_folder_name}/composer.json > ./composer.json;
+cat ${drupal_tmp_folder_name}/composer.lock > ./composer.lock;
 # Get shared folders
-cp -R ${drupal_tmp_folder_name}/modules ./modules;
-cp -R ${drupal_tmp_folder_name}/profiles ./profiles
-cp -R ${drupal_tmp_folder_name}/themes ./themes;
-cp -R ${drupal_tmp_folder_name}/sites ./sites;
+cp -R ${drupal_tmp_folder_name}/modules ./;
+cp -R ${drupal_tmp_folder_name}/profiles ./;
+cp -R ${drupal_tmp_folder_name}/themes ./;
+cp -R ${drupal_tmp_folder_name}/sites ./;
+# Get shared files
 cp ${drupal_tmp_folder_name}/robots.txt ./robots.txt;
 cp ${drupal_tmp_folder_name}/web.config ./web.config;
-# Create a composer.lock to prevent docker-compose error on volumes mount
-touch ./composer.lock;
 
 rm -rf tmp_drupal;
 
